9ab055e6bf3e6deeb382f903aa31dff3b0d39a78
Punit Sharma <punitcs0067@gmail.com>
2026-04-23 12:32:55 +0530

    Added profile pic upload to s3 bucket using boto3. COMPLETED!

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ async def lifespan(_app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-app.mount("/media", StaticFiles(directory="media"), name="media")
+# removed when app is using S3 to upload profile pics
+# app.mount("/media", StaticFiles(directory="media"), name="media")
 
 templates = Jinja2Templates(directory="templates")
 
